19b9c77ae1d83a3ac6a93b47e37d8e937bd404c0
samuel m <samm67363@gmail.com>
2026-06-30 21:57:46 +0530

    Fix firebase config to support Render env variable

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,6 +1,13 @@
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getAuth } = require("firebase-admin/auth");
-const serviceAccount = require("./serviceAccountKey.json");
+
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  serviceAccount = require("./serviceAccountKey.json");
+}
 
 initializeApp({
   credential: cert(serviceAccount),
