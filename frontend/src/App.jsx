32b29c275b4576d00466ddb0f3c4b52b55e1d895
samuel m <samm67363@gmail.com>
2026-07-04 19:53:39 +0530

    Fix route path in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,7 +29,10 @@ export default function App() {
         <Route path="/" element={<PrivateRoute><ChatPage /></PrivateRoute>} />
         <Route path="*" element={<Navigate to="/" replace />} />
         <Route path="/privacy" element={<PrivateRoute><PrivacySettingsPage /></PrivateRoute>} />
-        <Route path="/invite/:code" element={<PrivateRoute><InvitePage /></PrivateRoute>} />
+        <Route
+  path="/invite/:code"
+  element={<h1>INVITE PAGE IS REACHED</h1>}
+/>
       </Routes>
       
     </BrowserRouter>
